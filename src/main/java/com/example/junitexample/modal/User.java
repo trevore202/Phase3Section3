1f9b99181f5055fc505b99541c409691fe32be92
Trevor Evans <trevor.evans@hcl.com>
2021-01-13 23:30:56 -0600

    first commit

---
+++ src/main/java/com/example/junitexample/modal/User.java
@@ -0,0 +1,14 @@
+package com.example.junitexample.modal;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class User {
+	
+	private String userName;
+	private String password;
+	private String role;
+	
+}
